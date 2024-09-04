209fd589b493d261ab8ce9b1bce5b0a8b945fd5e
test <game360region@gmail.com>
2024-09-04 17:40:01 +0900

    first

---
+++ src/pages/HomeScreen.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>ホーム！！！</h1>
+    <input v-model="text">
+    <h3>{{ text }}</h3>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useHead } from 'unhead';
+import { headData } from './ts/head';
+
+useHead({
+    title: headData.home.title
+});
+
+const text = ref('')
+
+</script>
---
+++ src/pages/ts/head.ts
@@ -0,0 +1,5 @@
+export const headData = {
+    home: {
+        title: "ホーム"
+    }
+}
